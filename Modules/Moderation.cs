b196fe4201af13cadd72a5cee8e4603369be3c2c
Alexis Schapman <akitake@protonmail.com>
2022-07-02 10:33:28 +0200

    Create purge command

--- Modules/Moderation.cs
+++ Modules/Moderation.cs
@@ -1,16 +1,26 @@
 using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
+using Bot.Helpers;
 
 namespace Bot.Modules;
 
+[SlashRequireGuild]
 public class Moderation : ApplicationCommandModule
 {
-    [SlashCommand("test", "")]
-    [SlashRequirePermissions(Permissions.ManageMessages)]
-    [SlashRequireGuild]
-    public async Task TestCommand(InteractionContext context)
+    [SlashCommand("purge", "Deletes X amount of messages (1-100)")]
+    public async Task Purge(InteractionContext context, [Option("amount", "Amount to delete")] long x)
     {
-        await context.Channel.SendMessageAsync("Test");
+        await context.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+        x = Math.Clamp(x, 1, 99);
+        var messages = await context.Channel.GetMessagesAsync((int)x+1);
+        
+        await context.Channel.DeleteMessagesAsync(messages);
+
+        await context.CreateResponseAsync(
+            InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().WithContent($"**{x}** messages deleted."));
     }
 }
