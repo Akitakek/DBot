781f79a7fd514d1ab2cd9d5313f9e1fc502ef7fb
Alexis Schapman <akitake@protonmail.com>
2022-07-02 12:32:33 +0200

    Moved functions around

--- Modules/Info.cs
+++ Modules/Info.cs
@@ -36,27 +36,6 @@ public class Info : ApplicationCommandModule
 
         await context.FollowUpAsync(new DiscordFollowupMessageBuilder().AddEmbed(embed));
     }
-    
-    [SlashCommand("avatar", "Get a user's avatar")]
-    public async Task Avatar(InteractionContext context, [Option("member", "Target member")] DiscordUser user)
-    {
-        await context.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-
-        var guild = context.Guild;
-        var member = await guild.GetMemberAsync(user.Id);
-        var channels = await context.Guild.GetChannelsAsync();
-        var tier = guild.PremiumTier == PremiumTier.None ? "Lv0" : guild.PremiumTier.ToString().Replace("Tier_", "Lv");
-
-        var embed = new DiscordEmbedBuilder
-        {
-            Color = DBot.ThemeColor,
-            Title = $"{member.DisplayName}#{member.Discriminator}"
-        }
-        .WithImageUrl(member.GetAvatarUrl(ImageFormat.Png))
-        .Build();
-
-        await context.FollowUpAsync(new DiscordFollowupMessageBuilder().AddEmbed(embed));
-    }
 
     [SlashCommand("roleinfo", "View info about a server role")]
     public async Task Roleinfo(InteractionContext context, [Option("role", "Target role")] DiscordRole role)
@@ -76,6 +55,27 @@ public class Info : ApplicationCommandModule
             $"Hoisted: `{role.IsHoisted.ToYesNo()}`\n" + 
             $"Position: `{role.Position}`\n" +
             $"ID: `{role.Id}`\n", false)
+        .Build();
+
+        await context.FollowUpAsync(new DiscordFollowupMessageBuilder().AddEmbed(embed));
+    }
+
+    [SlashCommand("avatar", "Get a user's avatar")]
+    public async Task Avatar(InteractionContext context, [Option("member", "Target member")] DiscordUser user)
+    {
+        await context.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+        var guild = context.Guild;
+        var member = await guild.GetMemberAsync(user.Id);
+        var channels = await context.Guild.GetChannelsAsync();
+        var tier = guild.PremiumTier == PremiumTier.None ? "Lv0" : guild.PremiumTier.ToString().Replace("Tier_", "Lv");
+
+        var embed = new DiscordEmbedBuilder
+        {
+            Color = DBot.ThemeColor,
+            Title = $"{member.DisplayName}#{member.Discriminator}"
+        }
+        .WithImageUrl(member.GetAvatarUrl(ImageFormat.Png))
         .Build();
 
         await context.FollowUpAsync(new DiscordFollowupMessageBuilder().AddEmbed(embed));
