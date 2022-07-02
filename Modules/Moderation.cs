84204190512407312e40abbd5d2ef37ad988e1ba
Alexis Schapman <akitake@protonmail.com>
2022-07-02 08:33:51 +0200

    Add test command

--- Modules/Moderation.cs
+++ Modules/Moderation.cs
@@ -1,9 +1,16 @@
 using DSharpPlus;
 using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
 
 namespace Bot.Modules;
 
 public class Moderation : ApplicationCommandModule
 {
-    
+    [SlashCommand("test", "")]
+    [SlashRequirePermissions(Permissions.ManageMessages)]
+    [SlashRequireGuild]
+    public async Task TestCommand(InteractionContext context)
+    {
+        await context.Channel.SendMessageAsync("Test");
+    }
 }
