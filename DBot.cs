1758c069cc7eb39df1d67ed4d0aa0c0d7f74d47a
Alexis Schapman <akitake@protonmail.com>
2022-07-03 15:49:35 +0200

    Add base module  & init LiteDB

--- DBot.cs
+++ DBot.cs
@@ -3,8 +3,7 @@ using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.EventArgs;
 using Bot.Modules;
-using Bot.Types;
-using Newtonsoft.Json;
+using Bot.Helpers;
 
 namespace Bot;
 
@@ -16,39 +15,26 @@ public class DBot
 
     public async Task<DiscordShardedClient> Initialize(DiscordShardedClient client)
     {
+        // Init globally (across all shards)
         var commands = await client.UseSlashCommandsAsync();
+        commands.RegisterCommands<Base>();
         commands.RegisterCommands<Moderation>();
         commands.RegisterCommands<Info>();
 
-        // Init global
-        client.GuildAvailable += GuildAvailable;      
+        client.GuildAvailable += GuildAvailable;
+
+        if (!Directory.Exists(DB.Guild.DBDir))
+            Directory.CreateDirectory(DB.Guild.DBDir);  
 
         return client;
     }
 
-    public void InitializeShard(DiscordClient client, ShardConfig config)
-    {
-        if (config.modules == null) throw new Exception($"Error initializing shard {client.ShardId}. Error: config.modules == null");
-    }
-
     private async Task GuildAvailable(DiscordClient client, GuildCreateEventArgs args)
     {
+        // Init shard
         var guildId = args.Guild.Id;
 
-        // Initialize config file if not present
-        var shardConfigDir = $"{AppDir}/Shards/{guildId}";
-        var shardConfigPath = $"{AppDir}/Shards/{guildId}/config.json";
-        if (!File.Exists(shardConfigPath)) 
-        {
-            Directory.CreateDirectory(shardConfigDir);
-            File.Copy(DefaultConfigPath, shardConfigPath);
-        }
-
-        // Load config file
-        var json = await File.ReadAllTextAsync(shardConfigPath);
-        var config = JsonConvert.DeserializeObject<ShardConfig>(json);
-
-        if (config == null) throw new Exception($"Error initializing shard {client.ShardId}. Error: config == null");
-        InitializeShard(client, config);
+        // Initialize database (if it doesn't exist)
+        DB.Guild.Set(guildId);
     }
 }
