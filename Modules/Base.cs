26034f768fda496fa637e2a0fbab6bd48b9831d7
Alexis Schapman <akitake@protonmail.com>
2022-07-03 15:48:37 +0200

    Add Base Module (not disable-able)

---
+++ Modules/Base.cs
@@ -0,0 +1,97 @@
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+using Bot.Helpers;
+using Bot.Entities;
+using LiteDB;
+
+namespace Bot.Modules;
+
+public class Base : ApplicationCommandModule
+{
+    public enum ModuleActions
+    {
+        [ChoiceName("enable")]
+        Enable,
+        [ChoiceName("disable")]
+        Disable,
+        [ChoiceName("status")]
+        Status,
+    }
+
+    public enum Modules
+    {
+        [ChoiceName("Info")]
+        Info,
+        [ChoiceName("Moderation")]
+        Moderation,
+    }
+
+    [SlashCommand("module", "Interact with the bot's modules")]
+    [SlashRequirePermissions(Permissions.Administrator)]
+    [SlashRequireGuild]
+    public async Task Module
+    (
+        InteractionContext context,
+        [Option("action", "Action to execute")] ModuleActions action,
+        [Option("module", "Module to act on")] Modules module = Modules.Info
+    )
+    {
+        using (var db = new LiteDatabase($"{DB.Guild.DBDir}{context.Guild.Id}.db"))
+        {
+            var col = db.GetCollection<DBGuild>("guilds");
+            var dbGuild = col.FindOne(x => x.Id == context.Guild.Id);
+            
+            string response = "";
+            switch (action)
+            {
+                case ModuleActions.Enable:
+                {
+                    if (!dbGuild.EnabledModules.Contains(module.GetName()))
+                    {
+                        dbGuild.EnabledModules.Add(module.GetName());
+                        response = $"Module `{module.GetName()}` has been **enabled** for the server.";
+                    }
+                    else
+                        response = $"Module `{module.GetName()}` was already enabled.";
+
+                    await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().WithContent(response));
+                    break;
+                }
+                case ModuleActions.Disable:
+                {
+                    if (dbGuild.EnabledModules.Contains(module.GetName()))
+                    {
+                        dbGuild.EnabledModules.RemoveAll(x => x == module.GetName());
+                        response = $"Module `{module.GetName()}` has been **disabled** for the server.";
+                    }
+                    else
+                        response = $"Module `{module.GetName()}` was not enabled.";
+                        
+                    await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().WithContent(response));
+                    break;
+                }
+                case ModuleActions.Status:
+                {
+                    response = $"Enabled modules: `{string.Join(", ", dbGuild.EnabledModules.ToArray())}`";
+
+                    await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().WithContent(response));
+                    break;
+                }
+                default:
+                {
+                    response = $"**[ERROR]** Action misunderstood.";
+                    await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().WithContent(response));
+                    break;
+                }
+            }
+
+            col.Update(dbGuild);
+        }
+    }
+}
