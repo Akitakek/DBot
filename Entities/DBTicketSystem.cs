6912431178d29d1a3d024b9f013daa9dd2a248f3
Alexis Schapman <akitake@protonmail.com>
2022-07-06 06:50:08 +0200

    Fixes

--- Entities/DBTicketSystem.cs
+++ Entities/DBTicketSystem.cs
@@ -5,20 +5,20 @@ namespace Bot.Entities;
 public class DBTicketSystem 
 {
     public int Id { get; set; }
-    public ulong guildId { get; set; }
+    public ulong GuildId { get; set; }
 
-    public string embedTitle { get; set; } = "Tickets";
-    public string embedDescription { get; set; } = "Create a ticket by clicking the button below.";
-    public DiscordColor embedColor { get; set; } = DBot.ThemeColor;
+    public string EmbedTitle { get; set; } = "Tickets";
+    public string EmbedDescription { get; set; } = "Create a ticket by clicking the button below.";
+    public DiscordColor EmbedColor { get; set; } = DBot.ThemeColor;
 
-    public string buttonText { get; set; } = "Create ticket";
-    public DiscordComponentEmoji buttonEmoji { get; set; } = new DiscordComponentEmoji("✉️");
+    public string ButtonText { get; set; } = "Create ticket";
+    public DiscordComponentEmoji ButtonEmoji { get; set; } = new DiscordComponentEmoji("✉️");
 
     public ulong TicketsCategory { get; set; }
     public string TicketsPrefix { get; set; } = "ticket-";
-    public string TicketsMessage { get; set; } = "{@USER}";
+    public string TicketsMessage { get; set; } = "$USER";
 
-    public List<DBTicket> tickets = new List<DBTicket>();
+    public List<DBTicket> Tickets = new List<DBTicket>();
     public int NextTicketId { get; set; } = 0;
 
     public static DBTicketSystem Create
@@ -38,14 +38,17 @@ public class DBTicketSystem
         DBTicketSystem _dbTicketSystem = new DBTicketSystem
         {
             Id = _id,
-            guildId = _guildId
+            GuildId = _guildId
         };
 
-        if (_embedColor != null) _dbTicketSystem.embedColor = (DiscordColor)_embedColor;
-        if (_embedTitle != null) _dbTicketSystem.embedTitle = _embedTitle;
-        if (_embedDescription != null) _dbTicketSystem.embedDescription = _embedDescription;
-        if (_buttonText != null) _dbTicketSystem.buttonText = _buttonText;
-        if (_buttonEmoji != null) _dbTicketSystem.buttonEmoji = _buttonEmoji;
+        if (_embedTitle != null) _dbTicketSystem.EmbedTitle = _embedTitle;
+        if (_embedDescription != null) _dbTicketSystem.EmbedDescription = _embedDescription;
+        if (_embedColor != null) _dbTicketSystem.EmbedColor = (DiscordColor)_embedColor;
+        if (_buttonText != null) _dbTicketSystem.ButtonText = _buttonText;
+        if (_buttonEmoji != null) _dbTicketSystem.ButtonEmoji = _buttonEmoji;
+        if (_ticketsCategory != null) _dbTicketSystem.TicketsCategory = (ulong)_ticketsCategory;
+        if (_ticketsPrefix != null) _dbTicketSystem.TicketsPrefix = _ticketsPrefix;
+        if (_ticketsMessage != null) _dbTicketSystem.TicketsMessage = _ticketsMessage;
 
         return _dbTicketSystem;
     }
